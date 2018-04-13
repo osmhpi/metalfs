da3af54a8faa3be7f40ed33a1575c1a65e7830cd
Lukas Wenzel <lukas.wenzel@student.hpi.de>
2018-04-13 18:31:41 +0200

    WIP add filebuf load and store logic from other branch

--- src/metal_fpga/hw/mf_file.cpp
+++ src/metal_fpga/hw/mf_file.cpp
@@ -33,3 +33,113 @@ void mf_file_flush(mf_extmap_t & slot,
     //TODO-lw WRITE BLOCK map.current_pblock
 }
 
+static mf_bool_t mf_file_write_buffer(snap_membus_t * mem, mf_slot_state_t & slot)
+{
+    snap_membus_t line = 0;
+    ap_uint<MF_BLOCK_BYTE_OFFSET_W - ADDR_RIGHT_SHIFT> i_line = 0;
+    for (mf_block_count_t i_byte = 0; i_byte < MF_BLOCK_BYTES; ++i_byte) {
+        line = (line << 8) | slot.buffer[i_byte];
+        if (i_byte % ADDR_RIGHT_SHIFT == ADDR_RIGHT_SHIFT - 1) {
+            mem[i_line++] = line;
+        }
+    }
+    return MF_TRUE;
+}
+
+static mf_bool_t mf_file_read_buffer(snap_membus_t * mem, mf_slot_state_t & slot)
+{
+    snap_membus_t line = 0;
+    ap_uint<MF_BLOCK_BYTE_OFFSET_W - ADDR_RIGHT_SHIFT> i_line = 0;
+    for (mf_block_count_t i_byte = 0; i_byte < MF_BLOCK_BYTES; ++i_byte) {
+        if (i_byte % ADDR_RIGHT_SHIFT == 0) {
+            line = mem[i_line++];
+        }
+        slot.buffer[i_byte] = (line >> (MEMDW - 8)) & 0xff;
+        line = (line << 8);
+    }
+    return MF_TRUE;
+}
+
+static mf_bool_t mf_nvme_write_burst(snapu32_t *d_nvme,
+                 snapu64_t ddr_addr,
+                 snapu64_t ssd_lb_addr,
+                 snap_bool_t drive_id,
+                 snapu32_t num_of_blocks_to_transfer)
+{
+    snapu32_t status;
+
+    // Set card ddr address
+    // ddr_addr <= 4GB, so no high part.
+    ((volatile int*)d_nvme)[0] = ddr_addr & 0xFFFFFFFF;
+    ((volatile int*)d_nvme)[1] = 0x00000002;
+    //((volatile int*)d_nvme)[1] = (ddr_addr >> 32) & 0xFFFFFFFF;
+
+    // Set card ssd address
+    ((volatile int*)d_nvme)[2] = ssd_lb_addr & 0xFFFFFFFF;
+    ((volatile int*)d_nvme)[3] = (ssd_lb_addr >> 32) & 0xFFFFFFFF;
+
+    // Set number of blocks to transfer
+    ((volatile int*)d_nvme)[4] = num_of_blocks_to_transfer;
+
+    // Initiate ssd read
+    ((volatile int*)d_nvme)[5] = (drive_id == 0)? 0x11: 0x31;
+
+    // Poll the status register until the operation is finished
+    while(1) {
+        if((status = ((volatile int*)d_nvme)[1])) {
+            if(status & 0x10) {
+                return MF_TRUE;
+            } else {
+                return MF_FALSE;
+            }
+        }
+    }
+}
+
+static mf_bool_t mf_nvme_read_burst(snapu32_t *d_nvme,
+                  snapu64_t ddr_addr,
+                  snapu64_t ssd_lb_addr,
+                  snap_bool_t drive_id,
+                  snapu32_t num_of_blocks_to_transfer)
+{
+    snapu32_t status;
+
+    // Set card ddr address
+    // ddr_addr <= 4GB, so no high part.
+    ((volatile int*)d_nvme)[0] = ddr_addr & 0xFFFFFFFF;
+    ((volatile int*)d_nvme)[1] = 0x00000002;
+    //((volatile int*)d_nvme)[1] = (ddr_addr >> 32) & 0xFFFFFFFF;
+
+    // Set card ssd addres
+    ((volatile int*)d_nvme)[2] = ssd_lb_addr & 0xFFFFFFFF;
+    ((volatile int*)d_nvme)[3] = (ssd_lb_addr >> 32) & 0xFFFFFFFF;
+
+    // Set number of blocks to transfer
+    ((volatile int*)d_nvme)[4] = num_of_blocks_to_transfer;
+
+    // Initiate ssd read
+    ((volatile int*)d_nvme)[5] = (drive_id == 0)? 0x10: 0x30;
+
+    // Poll the status register until the operation is finished
+    while(1) {
+        if((status = ((volatile snapu32_t*)d_nvme)[1])) {
+            if(status & 0x10) {
+                return MF_TRUE;
+            } else {
+                return MF_FALSE;
+            }
+        }
+    }
+}
+
+static mf_bool_t mf_file_store_buffer(snap_membus_t * mem, snapu32_t * nvme_ctrl, mf_slot_state_t & slot)
+{
+    return mf_file_write_buffer(mem + MFB_ADDRESS(slot.block_buffer_address), slot) &&
+            mf_nvme_write_burst(nvme_ctrl, slot.block_buffer_address, slot.current_pblock, false, MF_BLOCK_BYTES/512);
+}
+
+static mf_bool_t mf_file_load_buffer(snap_membus_t * mem, snapu32_t * nvme_ctrl, mf_slot_state_t & slot)
+{
+    return mf_nvme_read_burst(nvme_ctrl, slot.block_buffer_address, slot.current_pblock, false, MF_BLOCK_BYTES/512) &&
+            mf_file_read_buffer(mem + MFB_ADDRESS(slot.block_buffer_address), slot);
+}
