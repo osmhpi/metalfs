335deb59486e0cade5e71d03aa1decb721a91cc9
Lukas Wenzel <lukas.wenzel@student.hpi.de>
2018-07-23 19:21:41 +0200

    use correct optstring when parsing blowfish keyfile option

--- src/metal_operators/op_blowfish_encrypt/operator.c
+++ src/metal_operators/op_blowfish_encrypt/operator.c
@@ -61,7 +61,7 @@ static const void* handle_opts(mtl_operator_invocation_args *args, uint64_t *len
             { "profile", no_argument, NULL, 'p' }
         };
 
-        int ch = getopt_long(args->argc, args->argv, "khp", long_options, &option_index);
+        int ch = getopt_long(args->argc, args->argv, "k:hp", long_options, &option_index);
         if (ch == -1)
             break;
 
