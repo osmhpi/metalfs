edf8a24b96070da7e6e1939a969f518f01e491d4
Robert Schmid <robert.schmid@hpi.uni-potsdam.de>
2019-09-11 11:22:31 +0200

    Fix OperatorRegistry setup during tests

--- example/test/base_test.cpp
+++ example/test/base_test.cpp
@@ -1,28 +1,14 @@
 #include "base_test.hpp"
 
 #include <libgen.h>
+#include <metal_pipeline/pipeline_runner.hpp>
 
 namespace metal {
 
 void PipelineTest::SetUp() {
 
-    // Determine the path to the operators directory
-
-    std::vector<char> buf(1024, 0);
-    ssize_t result = readlink("/proc/self/exe", &buf[0], buf.size());
-    if (result < 0)
-    {
-        throw std::runtime_error("Could not read /proc/self/exe");
-    }
-
-    const char *dir = dirname(buf.data());
-
-    // We assume that the executable resides at metal_fs/<cmake_build>/example_test
-    auto operatorsPath = std::string(dir) + "/../example/image/operators";
-
-    std::cout << operatorsPath << std::endl;
-
-    _registry = std::make_unique<OperatorRegistry>(operatorsPath);
+    auto info = SnapPipelineRunner::readImageInfo(0);
+    _registry = std::make_unique<OperatorRegistry>(info);
 }
 
 } // namespace metal
