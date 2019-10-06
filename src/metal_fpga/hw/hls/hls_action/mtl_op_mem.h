e5777e9b5a29cd91f901b21794a4008074799d13
Robert Schmid <r.schmid@outlook.com>
2019-10-06 08:34:52 +0000

    Fix NVMe transfer address mapping

--- src/metal_fpga/hw/hls/hls_action/mtl_op_mem.h
+++ src/metal_fpga/hw/hls/hls_action/mtl_op_mem.h
@@ -15,15 +15,28 @@ typedef struct axi_datamover_status {
 } axi_datamover_status_t;
 typedef hls::stream<axi_datamover_status_t> axi_datamover_status_stream_t;
 
+class DatamoverIbttStatus : public ap_uint<32> {
+public:
+    auto end_of_packet()         -> decltype(ap_uint<32>::operator[](31)) { return (*this)[31]; }
+    auto num_bytes_transferred() -> decltype(ap_uint<32>::range(30, 8))   { return this->range(30, 8); }
+};
+
 typedef struct axi_datamover_ibtt_status {
-    ap_uint<32> data;
+    DatamoverIbttStatus data;
     ap_uint<1> keep;
     ap_uint<1> last;
 } axi_datamover_ibtt_status_t;
 typedef hls::stream<axi_datamover_ibtt_status_t> axi_datamover_status_ibtt_stream_t;
 
-typedef ap_uint<103> axi_datamover_command_t;
-typedef hls::stream<axi_datamover_command_t> axi_datamover_command_stream_t;
+class DatamoverCommand : public ap_uint<103> {
+public:
+    auto end_of_frame()      -> decltype(ap_uint<103>::operator[](30)) { return (*this)[30]; }
+    auto axi_burst_type()    -> decltype(ap_uint<103>::operator[](23)) { return (*this)[23]; }
+    auto effective_address() -> decltype(ap_uint<103>::range(95, 32))  { return this->range(95, 32); }
+    auto transfer_bytes()    -> decltype(ap_uint<103>::range(22, 0))   { return this->range(22, 0); }
+};
+
+typedef hls::stream<DatamoverCommand> axi_datamover_command_stream_t;
 
 class NVMeCommand : public ap_uint<168> {
 public:
