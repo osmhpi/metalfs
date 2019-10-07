8e39cdba316001f575f2cc652acfa44f87ea5a15
Robert Schmid <r.schmid@outlook.com>
2019-10-07 13:20:33 +0000

    Make log verbosity configurable in tests

--- test/metal_pipeline_test/gtest_main.cpp
+++ test/metal_pipeline_test/gtest_main.cpp
@@ -8,7 +8,25 @@
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv) {
-    // spdlog::set_level(spdlog::level::debug);
     ::testing::InitGoogleTest(&argc, argv);
+
+    int verbose = 0;
+    for (int optind = 1; optind < argc && argv[optind][0] == '-'; optind++) {
+        size_t arglen = strlen(argv[optind]);
+        if (arglen >= 2 && argv[optind][1] == '-') break;
+        for (size_t i = 1; i < arglen; i++)
+            if (argv[optind][i] == 'v') verbose++;
+    }
+
+    if (verbose >= 3) {
+        spdlog::set_level(spdlog::level::trace);
+    } else if (verbose == 2) {
+        spdlog::set_level(spdlog::level::debug);
+    } else if (verbose == 1) {
+        spdlog::set_level(spdlog::level::info);
+    } else {
+        spdlog::set_level(spdlog::level::warn);
+    }
+
     return RUN_ALL_TESTS();
 }
