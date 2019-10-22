78e2008233cd7049b9d4cdd6692bf39b4780f456
Robert Schmid <robert.schmid@hpi.uni-potsdam.de>
2019-10-22 10:43:43 +0200

    File data source/sink: Add seek method

--- src/metal_filesystem_pipeline/file_data_source.hpp
+++ src/metal_filesystem_pipeline/file_data_source.hpp
@@ -5,24 +5,25 @@ namespace metal {
 
 class FileDataSource : public DataSource {
 
-  // Common API
- public:
- protected:
-   fpga::AddressType addressType() override { return fpga::AddressType::NVMe; }
+// Common API
+public:
+protected:
+  fpga::AddressType addressType() override { return fpga::AddressType::NVMe; }
 
-   void configure(SnapAction &action) override;
-   void finalize(SnapAction &action) override;
+  void configure(SnapAction &action) override;
+  void finalize(SnapAction &action) override;
+  void seek(uint64_t offset) { _address = offset; }
 
-   std::vector<mtl_file_extent> _extents;
+  std::vector<mtl_file_extent> _extents;
 
 
   // API to be used from PipelineStorage (extent list-based)
- public:
+public:
   explicit FileDataSource(std::vector<mtl_file_extent> &extents, uint64_t offset, uint64_t size);
 
 
   // API to be used when building file pipelines (filename-based)
- public:
+public:
   explicit FileDataSource(std::string filename, uint64_t offset, uint64_t size = 0);
   size_t reportTotalSize() override;
 
